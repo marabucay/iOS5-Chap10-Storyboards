b2779a80f2d3d048a77382f8b00913f6a0505c60
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-17 15:46:37 +0800

    Replacing old table view data source.

--- Simple Storyboard/Simple Storyboard/BIDStaticCellsController.m
+++ Simple Storyboard/Simple Storyboard/BIDStaticCellsController.m
@@ -35,12 +35,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSDate *now = [NSDate date];
+    dateLabel.text = [NSDateFormatter localizedStringFromDate:now
+                                                    dateStyle:NSDateFormatterLongStyle
+                                                    timeStyle:NSDateFormatterNoStyle];
+    timeLabel.text = [NSDateFormatter localizedStringFromDate:now
+                                                    dateStyle:NSDateFormatterNoStyle
+                                                    timeStyle:NSDateFormatterLongStyle];
 }
 
 - (void)viewDidUnload
@@ -80,33 +81,6 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
-    
-    // Configure the cell...
-    
-    return cell;
-}
 
 /*
 // Override to support conditional editing of the table view.

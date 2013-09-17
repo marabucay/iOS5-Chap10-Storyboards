8db493ca220efd217269321c346e1319b7926988
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-17 16:38:15 +0800

    Created new task list GUI.

---
+++ Seg Nav/Seg Nav/BIDTaskDetailController.h
@@ -0,0 +1,14 @@
+//
+//  BIDTaskDetailController.h
+//  Seg Nav
+//
+//  Created by Maria Alice C. Lim on 9/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDTaskDetailController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end

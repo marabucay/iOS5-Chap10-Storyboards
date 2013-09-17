b893e299d12292e9f7e624cc3554484c70cb2a74
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-17 15:48:41 +0800

    Created Seg Nav Project.

---
+++ Seg Nav/Seg Nav/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Seg Nav
+//
+//  Created by Maria Alice C. Lim on 9/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
